72adb8807922ff820f30db2bbba6f3d92ed0af19
unknown <johnzak80@gmail.com>
2022-07-04 20:07:00 -0400

    add middle ware

--- server.js
+++ server.js
@@ -7,16 +7,17 @@ const PORT = 8000
 
 let db,
     dbConnectionString = process.env.DB_STRING
-    dbName = 'sample_mflix',
+    dbName = 'sample_mflix', //has to be your own
     collection ='comments'
 
 MongoClient.connect(dbConnectionString)
     .then(client => {
         console.log('Connected to Database')
         db = client.db(dbName)
-        collection = db.collection('comments')
+        collection = db.collection('comments') //has to be your own
     })
 
+    //PORT - 8000adde
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
